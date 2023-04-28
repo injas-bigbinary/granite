8ef2f6d808f5e5863464d560fbb79617189c441b
INJAS <injas.mtp@bigbinary.com>
2023-04-28 15:37:27 +0530

    Implemented create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,19 @@ class TasksController < ApplicationController
   respond_to :html, :xml, :json
 
   def index
-    tasks = Task.all
-    render status: :ok, json: { tasks: }
+    @tasks = Task.all
+    render status: :ok, json: { tasks: @tasks }
   end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    render_notice(t("successfully_created"))
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:title)
+    end
 end
