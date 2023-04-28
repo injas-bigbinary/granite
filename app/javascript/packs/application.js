ae821f088da2985b5b5ff20185cd054f0a295782
INJAS <injas.mtp@bigbinary.com>
2023-04-28 12:11:50 +0530

    Added slug to task

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,8 @@
 import "stylesheets/application";
 
 // Support component names relative to this directory:
-var componentRequireContext = require.context("src", true);
-var ReactRailsUJS = require("react_ujs");
+const componentRequireContext = require.context("src", true);
+const ReactRailsUJS = require("react_ujs");
 
 // Makes React dev tools etc, work.
 ReactRailsUJS.mountComponents();
